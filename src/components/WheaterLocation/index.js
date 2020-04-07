862eb9e7a61c982079b6b234153f153dbac13572
diego <diegomotta18@gmail.com>
2020-04-07 10:54:58 -0300

    conected to api openweathermap

--- src/components/WheaterLocation/index.js
+++ src/components/WheaterLocation/index.js
@@ -4,18 +4,17 @@ import WheaterData from './WheaterData';
 import './style.css';
 import { SNOW, SUN } from '../../constants/wheaters';
 
-const data0 ={
+const location= 'Buenos Aires,ar';
+const api_key = '76c4a06e90c01a93ff185c8dadd7ec13';
+const api_wheater = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`
+//const api_wheater = 'http://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,ar&appid=76c4a06e90c01a93ff185c8dadd7ec13'
+ const data1 ={
     temperature: 20,
     wheaterState: SNOW,
     humidity: 10,
     wind: '10 m/s'
 }
-const data1 ={
-    temperature: 10,
-    wheaterState: SUN,
-    humidity: 20,
-    wind: '210 m/s'
-}
+
 
 class WheaterLocation extends Component{
     constructor(){
@@ -25,8 +24,39 @@ class WheaterLocation extends Component{
             city:'Buenos Aires',
             data : data1};
     }
+
+   getWheaterState = wheater =>{
+       return SUN
+   }
+
+    getData = (wheaterData)=>{
+        debugger;
+        const {humidity,temp} = wheaterData.main;
+        const {speed} = wheaterData.wind;
+        const wheaterState = this.getWheaterState(this.wheater)
+
+        const data ={
+            humidity,
+            temperature: temp,
+            wheaterState, 
+            wind: `${speed} m/s `
+        } 
+        return data;
+    }
+
     handleUpdateClick=()=>{
-        this.setState({data: data0});
+        fetch(api_wheater).then(data =>{
+            console.log(data)
+             return data.json();
+         }).then(wheater_data =>{
+            console.log(wheater_data);
+            const data = this.getData(wheater_data);
+            this.setState({data})
+         })
+        
+
+        //});
+        //this.setState({data: data0});
     }
 
     render =()=>
