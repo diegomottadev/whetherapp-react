18209a3e8f21a3f7e0db7bf11796ef866f4bc4c0
diego <diegomotta18@gmail.com>
2020-04-07 09:12:32 -0300

    manejando estado y cambiando a class component

--- src/components/WheaterLocation/WheaterData/index.js
+++ src/components/WheaterLocation/WheaterData/index.js
@@ -3,12 +3,26 @@ import WheaterTemperature from './WheaterTemperature';
 import WheaterExtraData from './WheaterExtraData';
 import {CLOUD,CLOUDY} from './../../../constants/wheaters';
 import './style.css';
+import { PropTypes } from 'prop-types';
 
-const WheaterData =()=>(
-    <div className="wheaterDataCont">
-        <WheaterTemperature temperature={20} wheaterStyle={CLOUD}/>
-        <WheaterExtraData humidity={80} wind={"10m/s"}/>
-    </div>
-);
+const WheaterData =({data})=>{
+    const {temperature,wheaterState,humidity,wind} = data;
+    return (
+        <div className="wheaterDataCont">
+            <WheaterTemperature temperature={temperature} wheaterStyle={wheaterState}/>
+            <WheaterExtraData humidity={humidity} wind={wind}/>
+        </div>
+    );
+
+ };
+
+ WheaterData.protoTypes = {
+     data: PropTypes.shape({
+         temperature: PropTypes.number.isRequired,
+         wheaterState: PropTypes.string.isRequired,
+         humidity: PropTypes.number.isRequired,
+         wind: PropTypes.string.isRequired
+     })
+ }
 
 export default WheaterData;
--- src/components/WheaterLocation/index.js
+++ src/components/WheaterLocation/index.js
@@ -2,12 +2,46 @@ import React,{Component} from 'react';
 import Location from './Location';
 import WheaterData from './WheaterData';
 import './style.css';
+import { SNOW, SUN } from '../../constants/wheaters';
 
-const WheaterLocation =()=>(
-    <div className="wheaterLocationCont">
-        <Location city={'Buenos Aires'}></Location>
-        <WheaterData ></WheaterData>
-    </div>
-);
+const data0 ={
+    temperature: 20,
+    wheaterState: SNOW,
+    humidity: 10,
+    wind: '10 m/s'
+}
+const data1 ={
+    temperature: 10,
+    wheaterState: SUN,
+    humidity: 20,
+    wind: '210 m/s'
+}
+
+class WheaterLocation extends Component{
+    constructor(){
+        super();
+        
+        this.state = {
+            city:'Buenos Aires',
+            data : data1};
+    }
+    handleUpdateClick=()=>{
+        this.setState({data: data0});
+    }
+
+    render =()=>
+
+        {
+            const {city,data} = this.state;
+            return (
+                <div className="wheaterLocationCont">
+                <Location city={city}></Location>
+                <WheaterData data={data}></WheaterData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+            );
+
+        }
+}
 
 export default WheaterLocation;
